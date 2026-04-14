6cb366c56f88e5f5bcf8d8e6427b0f1c9a5b2e2e
Syed-Shabok <syed.shabok@gmail.com>
2026-04-14 18:22:46 +0600

    Initialized Project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen font-semibold text-3xl text-blue-700 bg-blue-100">
+      <h1>Project Initiated.</h1>
+      <button className="btn btn-primary">Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
