0f6ad884313456f2eef81e934d70a4e0de05dbe0
Syed-Shabok <syed.shabok@gmail.com>
2026-04-16 06:34:24 +0600

    Implemented Timeline Page Interactions Filtering.

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FriendContext } from "../../context/FriendContextProvider";
 import TimelineCard from "../../components/ui/TimelineCard";
@@ -8,11 +8,28 @@ const Timeline = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [filterOption, setFilterOption] = useState("Filter Timeline");
 
-  const { interactions, setInteractions } = useContext(FriendContext);
+  const { interactions } = useContext(FriendContext);
+
+  const [displayInteractions, setDisplayInteractions] = useState([]);
+
+  useEffect(() => {
+    setDisplayInteractions(interactions);
+  }, [interactions]);
 
   const handleDropdownSelect = (option) => {
     setFilterOption(option);
     setIsOpen(false);
+
+    if (option === "All") {
+      setDisplayInteractions(interactions);
+      return;
+    }
+
+    const filteredList = interactions.filter(
+      (interaction) => interaction.type === option,
+    );
+
+    setDisplayInteractions(filteredList);
   };
 
   return (
@@ -30,6 +47,9 @@ const Timeline = () => {
             <MdKeyboardArrowDown size={22} />
           </div>
           <ul className="dropdown-content menu bg-base-100 rounded-box z-1 w-60 p-2 shadow-sm">
+            <li onClick={() => handleDropdownSelect("All")}>
+              <a>All</a>
+            </li>
             <li onClick={() => handleDropdownSelect("Call")}>
               <a>Call</a>
             </li>
@@ -46,13 +66,15 @@ const Timeline = () => {
       {/* Timeline Cards Container*/}
       <div className="flex flex-col gap-6 min-h-[65vh]">
         {interactions.length === 0 ? (
-          <div className="h-[65vh] flex flex-col items-center justify-center gap-5 bg-[#FFFFFF] rounded-lg shadow-sm border border-gray-200 text-gray-500">
+          <div className="h-[65vh] flex flex-col items-center justify-center gap-5 bg-[#FFFFFF] rounded-lg shadow-sm border border-gray-200 text-gray-500 px-5">
             <LiaUserFriendsSolid size={70} />
 
-            <h1 className="text-3xl">You currently have no interactions.</h1>
+            <h1 className="text-2xl md:text-3xl text-center">
+              You currently have no interactions.
+            </h1>
           </div>
         ) : (
-          interactions.map((interaction, index) => (
+          displayInteractions.map((interaction, index) => (
             <TimelineCard key={index} interaction={interaction} />
           ))
         )}
